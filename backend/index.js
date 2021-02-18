f8989f6f537a28a17f3d1040fea1b06bc8400cdd
VirtualVasquez <mxv647@med.miami.edu>
2021-02-17 22:36:58 -0500

    Node and Express setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>
+    res.send(`Our Soccer application is running on port ${PORT}`)
+)
+
+app.listen(PORT, () =>
+    console.log(`Your soccer serer is running on port ${PORT}`)
+)
