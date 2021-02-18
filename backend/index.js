fbf934eb3b32fda6d961da90e9723c19d5264d8d
VirtualVasquez <mxv647@med.miami.edu>
2021-02-18 08:51:06 -0500

    created the player schema

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+//mongo connection
+//this allows us to use a promise to connect to mongo
+////the promise will expect a reponse before it says it succeeded or not
+mongoose.Promise = global.Promise;
+//this syntax connects us
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser:true,
+    useUnifiedToplogy: true
+});
+
+// bodyparser setup
+app.use(bodyparser.urlencoded({extended: true}))//this allows us to pass the request and encoded it properly to use it
+app.use(bodyparser.json());
 
 app.get('/', (req, res) =>
     res.send(`Our Soccer application is running on port ${PORT}`)
