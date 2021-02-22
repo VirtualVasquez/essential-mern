1ed2e929b85a5f71372df0f055687460c3d73fa0
VirtualVasquez <mxv647@med.miami.edu>
2021-02-21 21:57:23 -0500

    Added the state and axios

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,25 +1,60 @@
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 import PlayerList from './Player/PlayerList'
 import PlayerSingle from './Player/PlayerSingle'
 import PlayerForm from './Player/PlayerForm'
 
 
-function App() {
-  return (
-    <div className="container-fluid">
-     <div className="row">
-       <div className="col s12">Menu</div>
-     </div>
-     <div className="row">
-       <div className="col s3"><PlayerList /></div>
-       <div className="col s9"><PlayerSingle /></div>
-     </div>
-     <div className="row">
-       <div className="col s12"><PlayerForm /></div>
-     </div>     
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {}
+    }
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
 
-    </div>
-  );
+  componentDidMount(){
+    const url = 'http://localhost:4000/players';
+
+    axios.get(url)
+      .then((response) => {
+        this.setState({
+          players: Response.data
+        })
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+  }
+
+  updateCurrentPlayer(item){
+    this.setState({
+      currentPlayer: item,
+    })
+
+  }
+
+  render(){
+    return (
+      <div className="container-fluid">
+      <div className="row">
+        <div className="col s12">Menu</div>
+      </div>
+      <div className="row">
+        <div className="col s3"><PlayerList  players={this.state.players}
+          updateCurrentPlayer={this.updateCurrentPlayer}/>
+        </div>
+        <div className="col s9"><PlayerSingle /></div>
+      </div>
+      <div className="row">
+        <div className="col s12"><PlayerForm /></div>
+      </div>     
+
+      </div>
+    );
+  }
 }
 
 export default App;
