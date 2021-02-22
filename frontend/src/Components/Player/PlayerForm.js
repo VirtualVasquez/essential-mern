4fbd8eb5ea3812fc5af06453e40f4aee879aa223
VirtualVasquez <mxv647@med.miami.edu>
2021-02-21 21:50:13 -0500

    Add the main page structure

---
+++ frontend/src/Components/Player/PlayerForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayerForm = () => {
+    return (
+        <div>
+            PlayerForm
+        </div>
+    );
+}
+ 
+
+export default PlayerForm;
+
---
+++ frontend/src/Components/Player/PlayerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayerList = () => {
+    return (
+        <div>
+            PlayerList
+        </div>
+    );
+}
+ 
+
+export default PlayerList ;
+
