617e224d64b8abfce40b0bf39e17ad28406b757d
VirtualVasquez <mxv647@med.miami.edu>
2021-02-22 20:34:56 -0500

    form base structure made, need `submitPlayer` function

--- frontend/src/Components/Player/PlayerForm.js
+++ frontend/src/Components/Player/PlayerForm.js
@@ -1,13 +1,42 @@
 import React from 'react';
 
-const PlayerForm = () => {
-    return (
-        <div>
-            PlayerForm
-        </div>
-    );
-}
- 
+ class PlayerForm extends React.Component{
+     constructor(props){
+         super(props);
+         this.state={ }
+     }
+     render(){
+         return(
+            <div className="row">
+                <h1 classNameName="center"> Add a new player</h1>
+                <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="firstName" ref="firstName" type="text" />
+                            <label htmlFor="firstName">First Name</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="lastName" ref="lastName" type="text" />
+                            <label htmlFor="lastName">First Name</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="phone" ref="phone" type="text" />
+                            <label htmlFor="phone">First Name</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="email" ref="email" type="text" />
+                            <label htmlFor="email">First Name</label>
+                        </div>
+                    </div>
+                    <button className="btn waves-effect waves-light" type="submit"
+                    name="action">Add Player</button>
+                </form>
+            </div>
+         )
+     }
+ }
 
 export default PlayerForm;
 
