186b169211f972e78fa8353bdaa6dad4a2a4a18d
VirtualVasquez <mxv647@med.miami.edu>
2021-02-22 19:00:32 -0500

    created single item component

--- frontend/src/Components/Player/PlayerSingle.js
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const Player = () => {
+const Player = (props) => {
     return (
-        <div>
-            Player Single
+    <div className="row">
+        <div className="col s12">
+          <div className="card">
+            <div className="card-image">
+              <span className="card-title">{props.player.firstName} {props.player.lastName}</span>
+            </div>
+            <div className="card-content">
+              <p>Phone: {props.player.phone} - Email: {props.player.email}</p>
+              <p>Strength: {props.player.strength} - Endurance: {props.player.endurance}</p>
+            </div>
+            <div className="card-action">
+                Team: {props.player.team}
+            </div>
+          </div>
         </div>
+    </div>
     );
 }
  
